230360be09294211ea66d8f71331ba0bd65c7d0c
Marvelous Imafidon <madogomi20131008@gmail.com>
2025-07-22 02:29:09 +0100

    I finished my JAMB calculator, I also started a new project which is similar to the JAMB calculator, it's called the GPA calculator, I also designed the central page that links to all my projects, now the central page looks acceptable. Lastly, I added a css beginner template in the LearnFullStack.

--- jamb-calculator/script.js
+++ jamb-calculator/script.js
@@ -1,22 +1,42 @@
-//select the id elements
-const form = document.getElementById("score-form");
-//grab all 5 subjects input fields
-const scoreInputs = document.querySelectorAll('input[name="score"]');
+const userName = document.getElementById("name-of-candidate");
 //grab the element in the html with the total id
-const totalDisplay = document.getElementById('total');
+const totalDisplay = document.getElementById("total");
 //grab the element in the html with the average id
-const averageDisplay = document.getElementById('average');
+const averageDisplay = document.getElementById("average");
 //grab the div that wraps the result section (with id="results")
+const btn = document.getElementById("submit-button");
 const resultsBox = document.getElementById('results');
-form.addEventListener("submit", function(e) {
-    e.preventDefault(); // Prevent the form from submitting normally
-    let total = 0; // Initialize total score
-    let count = 0; // Initialize count of valid scores
-    // we're trying to now actually calculate the total and average
-    scoreInputs.forEach(function(input) {
-          const value = input.value.trim(); // Get what the user typed and clean it
-          const score = parseFloat(value); // Convert the cleaned value to a number
-            if (!isNaN(score)) {
-    total += score;     // Add valid score to total
-    count++;            // Count it as a valid entry
-}
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+const fourth = document.getElementById("fourth");
+const fifth = document.getElementById("fifth");
+const shortNote = document.getElementById("short-note");
+
+
+
+
+
+
+btn.addEventListener("click", function(event){
+  event.preventDefault();
+    if (
+    isNaN(first.valueAsNumber) ||
+    isNaN(second.valueAsNumber) ||
+    isNaN(third.valueAsNumber) ||
+    isNaN(fourth.valueAsNumber) ||
+    isNaN(fifth.valueAsNumber) ||
+    userName.value.length < 3
+  ) {
+    alert("Please fill in all the correct inputs.");
+    return;
+  }
+
+  resultsBox.classList.remove("hidden");
+  
+  const valueOfInput =  first.valueAsNumber + second.valueAsNumber + third.valueAsNumber + fourth.valueAsNumber + fifth.valueAsNumber;
+  
+  totalDisplay.textContent = valueOfInput;
+  averageDisplay.textContent = valueOfInput / 5;
+  shortNote.textContent = "Mr/Mrs " + userName.value + ", your JAMB score is " + valueOfInput ;
+});
